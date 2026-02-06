0b0d4c9fe267d92ceb0eb8cc50351ed2e8409717
Laryssa Felix <felixllaryssa@gmail.com>
2026-02-06 08:51:02 -0300

    Update queries by weather, severity and time

--- Data Layer/gold/consultas.sql
+++ Data Layer/gold/consultas.sql
@@ -3,113 +3,160 @@
 -- ============================================================================
 -- Objetivo: Criar uma coluna de localização
 SELECT 
-    CONCAT(g.loc, ', ', g.ctr) AS localizacao_mapa,
-    
-    g.loc AS cidade,
+    t.evt_dat AS data_evento,
+    a.mak AS maker,
+    a.arc_cat AS categoria_aeronave,
     g.ctr AS pais,
-    t.evt_dat AS data_acidente,
+
+    CONCAT(g.loc, ', ', g.ctr) AS localizacao_mapa,
+    g.loc AS cidade,
     CONCAT(a.mak, ' ', a.mod) AS aeronave,
     s.inj_sev AS severidade,
     f.tot_fat_inj AS total_fatais
-
 FROM dw.fat_acc f
 JOIN dw.dim_geo g ON f.srk_geo = g.srk_geo
 JOIN dw.dim_sev s ON f.srk_sev = s.srk_sev
 JOIN dw.dim_tim t ON f.srk_tim = t.srk_tim 
-JOIN dw.dim_arc a ON f.srk_arc = a.srk_arc 
-
+JOIN dw.dim_arc a ON f.srk_arc = a.srk_arc
 WHERE g.loc IS NOT NULL 
   AND g.ctr IS NOT NULL
-  AND g.loc != '' 
-  AND g.ctr != '';
+
+  -- mapa
 
 -- ============================================================================
 -- 2.RELATÓRIO COM NÚMEROS GERAIS 
 -- ============================================================================
 -- Objetivo: Cartões com os números totais do relatório.
 SELECT 
-    COUNT(f.evt_ide) AS total_eventos,
-    SUM(f.tot_fat_inj + f.tot_ser_inj + f.tot_min_inj + f.tot_uni) AS total_vitimas,
-    SUM(f.tot_fat_inj) AS total_mortes,
-    ROUND((SUM(CAST(f.tot_fat_inj AS DECIMAL)) /  NULLIF(SUM(f.tot_fat_inj + f.tot_ser_inj + f.tot_min_inj + f.tot_uni), 0)) * 100, 2) AS taxa_letalidade_geral FROM dw.fat_acc f
+    t.evt_dat AS data_evento,
+    a.mak AS maker,
+    a.arc_cat AS categoria_aeronave,
+    g.ctr AS pais,
+
+    COUNT(f.evt_ide) AS total_acidentes,
+    SUM(f.tot_fat_inj) AS total_mortos,
+    ROUND(
+        AVG(
+            CAST(f.tot_fat_inj AS DECIMAL) /
+            NULLIF(
+                f.tot_fat_inj + f.tot_ser_inj + f.tot_min_inj + f.tot_uni,
+                0
+            )
+        ) * 100,
+        2
+    ) AS taxa_letalidade_media
+FROM dw.fat_acc f
+JOIN dw.dim_arc a ON f.srk_arc = a.srk_arc
+JOIN dw.dim_tim t ON f.srk_tim = t.srk_tim
+JOIN dw.dim_geo g ON f.srk_geo = g.srk_geo
+WHERE a.arc_cat IS NOT NULL
+  AND a.arc_cat <> 'UNKNOWN'
+GROUP BY 
+    t.evt_dat,
+    a.mak,
+    a.arc_cat,
+    g.ctr
+
+-- dados gerais
 
 -- ============================================================================
--- 3 TENDÊNCIA TEMPORAL DADOS CONSISTENTES DESDE 1982
+-- 3 SAZONALIDADE ANUAL
 -- ============================================================================
--- Objetivo: Mostrar a evolução a partir de 1982, início da série consistente.
+-- Objetivo: identificar os anos com o maior número de acidentes.
 SELECT 
-    CAST(EXTRACT(YEAR FROM t.evt_dat) AS INTEGER) AS ano,
+    t.evt_dat AS data_evento,
+    EXTRACT(YEAR FROM t.evt_dat)::INT AS ano,
+    a.mak AS maker,
+    a.arc_cat AS categoria_aeronave,
+    g.ctr AS pais,
+
     COUNT(f.evt_ide) AS qtd_acidentes,
     SUM(f.tot_fat_inj) AS qtd_fatais
 FROM dw.fat_acc f
 JOIN dw.dim_tim t ON f.srk_tim = t.srk_tim
-WHERE CAST(EXTRACT(YEAR FROM t.evt_dat) AS INTEGER) >= 1982 
-GROUP BY CAST(EXTRACT(YEAR FROM t.evt_dat) AS INTEGER)
-ORDER BY ano;
+JOIN dw.dim_arc a ON f.srk_arc = a.srk_arc
+JOIN dw.dim_geo g ON f.srk_geo = g.srk_geo
+WHERE EXTRACT(YEAR FROM t.evt_dat) >= 1982
+GROUP BY t.evt_dat, ano, a.mak, a.arc_cat, g.ctr
+ORDER BY ano
+
+--sazonalidade anual
 
 -- ============================================================================
 -- 4. FASES DE VOO CRÍTICAS (RANKING)
 -- ============================================================================
 -- Objetivo: identificar o momento mais perigoso.
-WITH fase_ranking AS (
-    SELECT 
-        p.brd_phs_off_flt AS fase,
-        COUNT(f.evt_ide) AS ocorrencias,
-        SUM(f.tot_fat_inj) AS total_fatais
-    FROM dw.fat_acc f
-    JOIN dw.dim_flt_phs p ON f.srk_flt_phs = p.srk_flt_phs
-    WHERE p.brd_phs_off_flt IS NOT NULL 
-      AND p.brd_phs_off_flt != 'UNKNOWN'  
-    GROUP BY p.brd_phs_off_flt
-)
-SELECT * FROM fase_ranking
-ORDER BY ocorrencias DESC
-LIMIT 10;
+SELECT 
+    t.evt_dat AS data_evento,
+    a.mak AS maker,
+    a.arc_cat AS categoria_aeronave,
+    g.ctr AS pais,
+
+    p.brd_phs_off_flt AS fase,
+    COUNT(f.evt_ide) AS ocorrencias,
+    SUM(f.tot_fat_inj) AS total_fatais
+FROM dw.fat_acc f
+JOIN dw.dim_flt_phs p ON f.srk_flt_phs = p.srk_flt_phs
+JOIN dw.dim_tim t ON f.srk_tim = t.srk_tim
+JOIN dw.dim_arc a ON f.srk_arc = a.srk_arc
+JOIN dw.dim_geo g ON f.srk_geo = g.srk_geo
+WHERE p.brd_phs_off_flt IS NOT NULL
+  AND p.brd_phs_off_flt <> 'UNKNOWN'
+GROUP BY t.evt_dat, a.mak, a.arc_cat, g.ctr, p.brd_phs_off_flt
+
+-- fases do voo
 
 -- ============================================================================
 -- 5. RANKING DE FABRICANTES (TOP 10)
 -- ============================================================================
 -- Objetivo: Listar os fabricantes com mais ocorrências.
-WITH ranking_fab AS (
-    SELECT 
-        a.mak AS fabricante,
-        COUNT(f.evt_ide) AS qtd_acidentes,
-        DENSE_RANK() OVER (ORDER BY COUNT(f.evt_ide) DESC) AS rank_pos
-    FROM dw.fat_acc f
-    JOIN dw.dim_arc a ON f.srk_arc = a.srk_arc
-    WHERE a.mak IS NOT NULL
-    GROUP BY a.mak
-)
 SELECT 
-    fabricante, 
-    qtd_acidentes 
-FROM ranking_fab
-WHERE rank_pos <= 10;
+    t.evt_dat AS data_evento,
+    a.mak AS maker,
+    a.arc_cat AS categoria_aeronave,
+    g.ctr AS pais,
+
+    COUNT(f.evt_ide) AS qtd_acidentes
+FROM dw.fat_acc f
+JOIN dw.dim_arc a ON f.srk_arc = a.srk_arc
+JOIN dw.dim_tim t ON f.srk_tim = t.srk_tim
+JOIN dw.dim_geo g ON f.srk_geo = g.srk_geo
+WHERE a.mak IS NOT NULL
+GROUP BY t.evt_dat, a.mak, a.arc_cat, g.ctr
+
+-- ranking de fab
 
 -- ============================================================================
 -- 6. SAZONALIDADE MENSAL 
 -- ============================================================================
 -- Objetivo: Identificar se existem meses mais perigosos no ano.
-WITH mensal AS (
-    SELECT 
-        TO_CHAR(t.evt_dat, 'MM') AS num_mes,
-        TO_CHAR(t.evt_dat, 'Month') AS nome_mes,
-        COUNT(f.evt_ide) AS total_eventos
-    FROM dw.fat_acc f
-    JOIN dw.dim_tim t ON f.srk_tim = t.srk_tim
-    GROUP BY TO_CHAR(t.evt_dat, 'MM'), TO_CHAR(t.evt_dat, 'Month')
-)
 SELECT 
-    nome_mes,
-    total_eventos
-FROM mensal
-ORDER BY num_mes;
+    t.evt_dat AS data_evento,
+    a.mak AS maker,
+    a.arc_cat AS categoria_aeronave,
+    g.ctr AS pais,
+
+    TO_CHAR(t.evt_dat, 'MM') AS num_mes,
+    TRIM(TO_CHAR(t.evt_dat, 'Month')) AS nome_mes,
+    COUNT(f.evt_ide) AS total_eventos
+FROM dw.fat_acc f
+JOIN dw.dim_tim t ON f.srk_tim = t.srk_tim
+JOIN dw.dim_arc a ON f.srk_arc = a.srk_arc
+JOIN dw.dim_geo g ON f.srk_geo = g.srk_geo
+GROUP BY t.evt_dat, a.mak, a.arc_cat, g.ctr, num_mes, nome_mes
+
+-- sazonalidade mensal
 
 -- ============================================================================
 -- 7. MATRIZ DE RISCO (TOP 5 FINALIDADES x SEVERIDADE)
 -- ============================================================================
 -- Objetivo: Heatmap focado nas categorias principais para comparar risco real.
 SELECT 
+    t.evt_dat AS data_evento,
+    a.mak AS maker,
+    a.arc_cat AS categoria_aeronave,
+    g.ctr AS pais,
+
     o.prp_off_flt AS finalidade,
     CASE 
         WHEN s.inj_sev IN ('Fatal', 'Non-Fatal') THEN s.inj_sev
@@ -119,83 +166,117 @@ SELECT
 FROM dw.fat_acc f
 JOIN dw.dim_opt o ON f.srk_opt = o.srk_opt
 JOIN dw.dim_sev s ON f.srk_sev = s.srk_sev
-WHERE o.prp_off_flt IN ('Personal', 'Instructional', 'Business', 'Aerial Application', 'Executive/corporate')
-GROUP BY o.prp_off_flt, 
-         CASE 
-            WHEN s.inj_sev IN ('Fatal', 'Non-Fatal') THEN s.inj_sev
-            ELSE 'Incident/Minor' 
-         END
-ORDER BY quantidade DESC;
-
-
--- ============================================================================
--- 8. ANÁLISE "WEEKEND WARRIOR" 
--- ============================================================================
--- Objetivo: Comparação do Dia Útil vs Fim de Semana.
-WITH analise_semanal AS (
-    SELECT 
-        TO_CHAR(t.evt_dat, 'Day') AS nome_dia,
-        CAST(TO_CHAR(t.evt_dat, 'D') AS INTEGER) AS num_dia,
-        CASE 
-            WHEN CAST(TO_CHAR(t.evt_dat, 'D') AS INTEGER) IN (1, 7) THEN 'Fim de Semana'
-            ELSE 'Dia de Semana'
-        END AS tipo_dia,
-        COUNT(f.evt_ide) AS total_acidentes,
-        SUM(f.tot_fat_inj) AS total_mortes
-    FROM dw.fat_acc f
-    JOIN dw.dim_tim t ON f.srk_tim = t.srk_tim
-    GROUP BY t.evt_dat
+JOIN dw.dim_tim t ON f.srk_tim = t.srk_tim
+JOIN dw.dim_arc a ON f.srk_arc = a.srk_arc
+JOIN dw.dim_geo g ON f.srk_geo = g.srk_geo
+WHERE o.prp_off_flt IN (
+    'Personal',
+    'Instructional',
+    'Business',
+    'Aerial Application',
+    'Executive/corporate'
 )
-SELECT 
-    nome_dia,
-    num_dia,
-    tipo_dia,
-    SUM(total_acidentes) as qtd_acidentes,
-    ROUND(AVG(total_mortes), 2) as media_mortes_por_dia
-FROM analise_semanal
-GROUP BY nome_dia, num_dia, tipo_dia
-ORDER BY num_dia;
+GROUP BY 
+    t.evt_dat,
+    a.mak,
+    a.arc_cat,
+    g.ctr,
+    o.prp_off_flt,
+    CASE 
+        WHEN s.inj_sev IN ('Fatal', 'Non-Fatal') THEN s.inj_sev
+        ELSE 'Incident/Minor' 
+    END
 
+-- matriz de risco 
 
 -- ============================================================================
--- 9. CONFIABILIDADE TECNOLÓGICA (TIPO DE MOTOR) 
+-- 8. CLASSIFICAÇÃO TEMPORAL DE ACIDENTES FATAIS E NÃO FATAIS 
 -- ============================================================================
--- Objetivo: Comparar risco fatal entre tipos de motor 
-WITH motor_stats AS (
-    SELECT 
-        a.eng_typ AS tipo_motor,
-        COUNT(f.evt_ide) AS qtd_eventos,
-        SUM(f.tot_fat_inj) AS qtd_fatais,
-        SUM(f.tot_uni + f.tot_fat_inj + f.tot_ser_inj + f.tot_min_inj) AS total_pessoas
-    FROM dw.fat_acc f
-    JOIN dw.dim_arc a ON f.srk_arc = a.srk_arc
-    WHERE a.eng_typ NOT IN ('UNKNOWN' )
-    GROUP BY a.eng_typ
-)
-SELECT 
-    tipo_motor,
-    qtd_eventos,
-    qtd_fatais,
-    ROUND((CAST(qtd_fatais AS DECIMAL) / NULLIF(total_pessoas, 0)) * 100, 2) AS pct_risco_fatal
-FROM motor_stats
-WHERE qtd_eventos > 50 
-ORDER BY pct_risco_fatal DESC;
+-- Objetivo: Classificar os acidentes aéreos ao longo dos anos em fatais e
+-- não fatais
+SELECT
+    -- colunas de filtro (BI)
+    t.evt_dat AS data_evento,
+    a.mak AS maker,
+    a.arc_cat AS categoria_aeronave,
+    g.ctr AS pais,
 
+    -- eixo do gráfico
+    EXTRACT(YEAR FROM t.evt_dat) AS ano,
+
+    -- métricas
+    SUM(CASE WHEN f.tot_fat_inj > 0 THEN 1 ELSE 0 END) AS qtd_fatal,
+    SUM(CASE WHEN f.tot_fat_inj = 0 THEN 1 ELSE 0 END) AS qtd_nao_fatal
+FROM dw.fat_acc f
+JOIN dw.dim_tim t
+    ON f.srk_tim = t.srk_tim
+JOIN dw.dim_arc a
+    ON f.srk_arc = a.srk_arc
+JOIN dw.dim_geo g
+    ON f.srk_geo = g.srk_geo
+GROUP BY
+    t.evt_dat,
+    a.mak,
+    a.arc_cat,
+    g.ctr,
+    EXTRACT(YEAR FROM t.evt_dat)
+ORDER BY ano
+
+-- fatalXnfatal
+
+-- ============================================================================
+-- 9.  DISTRIBUIÇÃO DOS ACIDENTES AÉREOS POR CONDIÇÃO METEOROLÓGICA 
+-- ============================================================================
+-- Objetivo: Analisar a distribuição dos acidentes aéreos de acordo com as
+-- condições meteorológicas
+SELECT 
+    -- colunas de filtro (BI)
+    t.evt_dat AS data_evento,
+    a.mak AS maker,
+    a.arc_cat AS categoria_aeronave,
+    g.ctr AS pais,
+
+    -- dimensão principal do gráfico
+    w.wth_con AS condicao_meteorologica,
+
+    -- métrica
+    COUNT(f.evt_ide) AS quantidade
+FROM dw.fat_acc f
+JOIN dw.dim_wth w
+    ON f.srk_wth = w.srk_wth
+JOIN dw.dim_tim t
+    ON f.srk_tim = t.srk_tim
+JOIN dw.dim_arc a
+    ON f.srk_arc = a.srk_arc
+JOIN dw.dim_geo g
+    ON f.srk_geo = g.srk_geo
+WHERE w.wth_con IN ('VMC', 'IMC', 'UNKNOWN')
+GROUP BY
+    t.evt_dat,
+    a.mak,
+    a.arc_cat,
+    g.ctr,
+    w.wth_con
+
+-- metereological
 
 -- ============================================================================
 -- 10. RISCO POR CATEGORIA DE AERONAVE
 -- ============================================================================
 -- Objetivo: Comparar volume e letalidade entre diferentes categorias
 SELECT 
-    a.arc_cat AS categoria,
-    COUNT(f.evt_ide) AS total_acidentes,
-    SUM(f.tot_fat_inj) AS total_mortos,
-    -- Taxa de Letalidade média por categoria
-    ROUND(AVG(CAST(f.tot_fat_inj AS DECIMAL) / NULLIF(f.tot_fat_inj + f.tot_ser_inj + f.tot_min_inj + f.tot_uni, 0)) * 100, 2) AS taxa_letalidade
+    t.evt_dat AS data_evento,
+    a.mak AS maker,
+    a.arc_cat AS categoria_aeronave,
+    g.ctr AS pais,
+
+    COUNT(f.evt_ide) AS total_eventos,
+    SUM(f.tot_fat_inj + f.tot_ser_inj + f.tot_min_inj + f.tot_uni) AS total_vitimas,
+    SUM(f.tot_fat_inj) AS total_mortes
 FROM dw.fat_acc f
+JOIN dw.dim_tim t ON f.srk_tim = t.srk_tim
 JOIN dw.dim_arc a ON f.srk_arc = a.srk_arc
-WHERE a.arc_cat IS NOT NULL 
-  AND a.arc_cat != 'UNKNOWN' 
-GROUP BY a.arc_cat
-HAVING COUNT(f.evt_ide) > 50
-ORDER BY total_acidentes DESC;
+JOIN dw.dim_geo g ON f.srk_geo = g.srk_geo
+GROUP BY t.evt_dat, a.mak, a.arc_cat, g.ctr
+
+-- aeronave
